5b76ff963967c71a7b8019fe1daba7a91615e7ad
rastatay <j.rendon@sesametime.com>
2022-06-22 12:39:09 +0200

    create if

---
+++ ejercicioTema4/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeroIf = 3;
+
+        if(numeroIf > 0) {
+            System.out.println("Este numero es positivo");
+        }else if(numeroIf == 0){
+            System.out.println("Este numero es cer0");
+        }else {
+            System.out.println("Este numero es negativo");
+        }
+    }
+}
