07296d3692878461df63c2897f4bba1e67d2fcca
rastatay <j.rendon@sesametime.com>
2022-06-22 12:57:16 +0200

    create while

--- ejercicioTema4/src/Main.java
+++ ejercicioTema4/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int numeroIf = 3;
+        int numeroWhile = 0;
 
+        //Condicional If
         if(numeroIf > 0) {
             System.out.println("Este numero es positivo");
         }else if(numeroIf == 0){
@@ -9,5 +11,12 @@ public class Main {
         }else {
             System.out.println("Este numero es negativo");
         }
+         //Bucle While inferior a 3
+        while(numeroWhile <= 3) {
+
+            System.out.println(numeroWhile);
+            numeroWhile = numeroWhile + 1;
+        }
+
     }
 }
