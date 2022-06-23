f1c965db039024d1836076f0db5fdd71b5168409
rastatay <j.rendon@sesametime.com>
2022-06-23 10:55:36 +0200

    create for and switch

--- ejercicioTema4/src/Main.java
+++ ejercicioTema4/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         int numeroIf = 3;
         int numeroWhile = 0;
         int numeroDo = 3;
+        var estacion = "sfs";
 
         //Condicional If
         if(numeroIf > 0) {
@@ -18,11 +19,38 @@ public class Main {
             System.out.println(numeroWhile);
             numeroWhile = numeroWhile + 1;
         }
+
         System.out.println("Finaliza el While");
         //Bucle doWhile
         do{
             System.out.println(numeroDo);
             numeroDo = numeroDo -1;
         }while(numeroDo > 3);
+
+        System.out.println("Finaliza el doWhile");
+        //Bucle For
+        for (var numeroFor = 0;numeroFor <= 3; numeroFor++) {
+            System.out.println(numeroFor);
+        }
+
+        System.out.println("Finaliza el For");
+        //Switch
+        switch (estacion){
+            case "Primavera":
+                System.out.println("Primavera");
+                break;
+            case "Verano":
+                System.out.println("Verano");
+                break;
+            case "Otoño":
+                System.out.println("Otoño");
+                break;
+            case "Invierno":
+                System.out.println("Invierno");
+                break;
+            default:
+                System.out.println("Esto no es una estación");
+                break;
+        }
     }
 }
