42c48a797568d5b3cc2697d479e2ddc81b81a70b
rastatay <j.rendon@sesametime.com>
2022-06-24 19:43:14 +0200

    create setters & getters class Persona

--- ejercicioTema8/src/Main.java
+++ ejercicioTema8/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        Persona PersonaUno = new Persona();
+        PersonaUno.setEdad(119);
+        PersonaUno.setNombre("Kane Tanaka");
+        PersonaUno.setTelefono(626622873);
 
+        System.out.println("Me llamo " + PersonaUno.getNombre() + ", tengo " + PersonaUno.getEdad() + " años" + " y mi número de telefono es: " + PersonaUno.getTelefono());
     }
 }
---
+++ ejercicioTema8/src/Persona.java
@@ -0,0 +1,25 @@
+public class Persona {
+    private int edad;
+    private String nombre;
+    private int telefono;
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+    public int getEdad() {
+        return this.edad;
+    }
+    public String getNombre() {
+        return this.nombre;
+    }
+    public int getTelefono() {
+        return this.telefono;
+    }
+
+}
