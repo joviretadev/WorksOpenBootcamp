916098a5cd458add40a9df4018f1a74b526bde21
rastatay <j.rendon@sesametime.com>
2022-06-26 21:05:28 +0200

    exercise Tema 9 complete

--- ejercicioTema9/src/Main.java
+++ ejercicioTema9/src/Main.java
@@ -1,87 +1,33 @@
 public class Main {
     public static void main(String[] args) {
+
         Cliente clienteUno = new Cliente();
         Trabajador trabajadorUno = new Trabajador();
-        clienteUno.setEdad(50);
-        clienteUno.setNombre("Tomás");
-        clienteUno.setTelefono(666995883);
-        clienteUno.setCredito(17500);
 
-        trabajadorUno.setEdad(37);
-        trabajadorUno.setNombre("Lorenzo");
-        trabajadorUno.setTelefono(692038747);
-        trabajadorUno.setSalario(1500);
+        clienteUno.edad = 50;
+        clienteUno.nombre = "Tomás";
+        clienteUno.telefono = 666995883;
+        clienteUno.credito = 17500;
 
-        System.out.println(clienteUno.getNombre() +" "+ clienteUno.getEdad() +" "+  clienteUno.getTelefono() +" "+  clienteUno.getCredito());
-        System.out.println(trabajadorUno.getNombre() +" "+ trabajadorUno.getEdad() +" "+  trabajadorUno.getTelefono() +" "+  trabajadorUno.getSalario());
+        trabajadorUno.edad = 37;
+        trabajadorUno.nombre = "Lorenzo";
+        trabajadorUno.telefono = 692038747;
+        trabajadorUno.salario = 1500;
+
+        System.out.println(clienteUno.nombre +" "+ clienteUno.edad +" años, con numero de telefono: "+  clienteUno.telefono +". Saldo: "+  clienteUno.credito +"€");
+        System.out.println(trabajadorUno.nombre +" "+ trabajadorUno.edad +" años, con numero de telefono: "+  trabajadorUno.telefono +". Salario: "+  trabajadorUno.salario+"€");
     }
 }
-   abstract class Persona {
-        int edad;
-        String nombre;
-        int telefono;
+class Persona{
+    int edad;
+    int telefono;
+    String nombre;
+}
 
-        abstract public void  setEdad(int edad);
-        abstract public void setNombre(String nombre);
-        abstract public void setTelefono(int telefono);
+class Cliente extends Persona{
+    float credito;
+}
 
-        abstract public int getEdad();
-        abstract public String getNombre();
-        abstract public int getTelefono();
-    }
-    class Cliente extends Persona {
-        int credito;
-        public void setEdad(int edad) {
-            this.edad = edad;
-        }
-        public int getEdad() {
-            return this.edad;
-        }
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-        }
-        public String getNombre() {
-            return this.nombre;
-        }
-        public void setTelefono(int telefono) {
-            this.telefono = telefono;
-        }
-        public int getTelefono() {
-            return this.telefono;
-        }
-        public void setCredito(int credito) {
-            this.credito = credito;
-        }
-        public int getCredito() {
-            return this.credito;
-        }
-
-    }
-    class Trabajador extends Persona {
-        float salario;
-
-        public void setSalario(float salario) {
-            this.salario = salario;
-        }
-        public float getSalario() {
-            return this.salario;
-        }
-        public void setEdad(int edad) {
-            this.edad = edad;
-        }
-        public int getEdad() {
-            return this.edad;
-        }
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-        }
-        public String getNombre() {
-            return this.nombre;
-        }
-        public void setTelefono(int telefono) {
-            this.telefono = telefono;
-        }
-        public int getTelefono() {
-            return this.telefono;
-        }
-    }
+class Trabajador extends Persona {
+    float salario;
+}
